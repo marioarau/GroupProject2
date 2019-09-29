0d40c548971422483585f87b61664561f32f4b73
marioarau <aztecanet@gmail.com>
2019-09-29 14:50:23 -0700

    added validations to the tables;

--- models/unit.js
+++ models/unit.js
@@ -1,17 +1,23 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var Unit = sequelize.define("Unit", {
     landLordId: DataTypes.INTEGER,
     bedrooms: DataTypes.STRING(10),
     baths: DataTypes.STRING(10),
     avgSqFt: DataTypes.INTEGER,
-    availability: DataTypes.STRING(15),
+    availability: {
+      type: Sequelize.ENUM,
+      values: ['Available Now', 'Available Soon', 'Waiting List', 'Not Available']
+    },
     address: DataTypes.STRING(80),
     city: DataTypes.STRING(35),
     state: DataTypes.STRING(2),
     zip: DataTypes.STRING(10),
     phone: DataTypes.STRING(25),
     desc: DataTypes.TEXT,
-    type: DataTypes.STRING(50)
+    type: {
+      type: Sequelize.ENUM,
+      values: ['Apartment', 'House', 'Condo', 'Townhouse']
+    }
   });
   return Unit;
 };
