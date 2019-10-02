f86aa4741ffbd3850c02085d1b328e1a5e036260
koa2019 <no-reply>
2019-10-01 19:30:46 -0700

    added a api get call with parameters for rent, bedroom & city

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,9 @@
 // import entire models folder
 var db = require("../models");
 
+// Necessary to find mix & max rent
+const Op = db.Sequelize.Op;
+
 // export this function that's passing an express server instance as an arguement
 module.exports = function(app) {
 
@@ -41,7 +44,6 @@ module.exports = function(app) {
     });
 
     // get route that returns all posts by zip code
-    // app.get("/api/units/city/:city", function(req, res) {
     app.get("/api/units/zip/:zip", function(req, res) {
 
         console.log(req);
@@ -80,15 +82,27 @@ module.exports = function(app) {
     });
 
     // get route for bedrooms & city
-    app.get("/api/units/search/:search", function(req, res) {
-        console.log(req.params)
+    // POSTMAN localhost:3000/api/fs/bedrooms/1/city/Los Angeles/rentlow/900/renthigh/1200
+    app.get("/api/fs/bedrooms/:bedrooms/city/:city/rentlow/:rent1/renthigh/:rent2", function(req, res) {
+        console.log("bedrooms: ", req.params.bedrooms);
+        console.log("city: ", req.params.city);
+        console.log("rentlow: ", req.params.rent1);
+        console.log("renthigh: ", req.params.rent2);
         db.Unit.findAll({
-            where: {
-                bedrooms: req.params.bedrooms,
-                city: req.params.city,
-                rent: req.params.rent
-            }
-        });
+                where: {
+                    bedrooms: req.params.bedrooms,
+                    city: req.params.city,
+                    rent: {
+                        [Op.between]: [req.params.rent1, req.params.rent2]
+                    }
+                }
+            })
+            .then(function(results) {
+                res.json(results);
+            })
+            .catch(function(err) {
+                res.status(500);
+            });
     });
 
     // post route for saving a new unit to database
@@ -99,28 +113,29 @@ module.exports = function(app) {
         // create() requires an object describing the new data we're adding to table
         db.Unit.create({
 
-            // landLordId: req.body.id,
-            title: req.body.title,
-            rent: req.body.rent,
-            bedrooms: req.body.bedrooms,
-            baths: req.body.baths,
-            avgSqFt: req.body.avgSqFt,
-            availability: req.body.availability,
-            address: req.body.address,
-            city: req.body.city,
-            state: req.body.state,
-            zip: req.body.zip,
-            phone: req.body.phone,
-            desc: req.body.desc,
-            type: req.body.type
+                // landLordId: req.body.id,
+                title: req.body.title,
+                rent: req.body.rent,
+                bedrooms: req.body.bedrooms,
+                baths: req.body.baths,
+                avgSqFt: req.body.avgSqFt,
+                availability: req.body.availability,
+                address: req.body.address,
+                city: req.body.city,
+                state: req.body.state,
+                zip: req.body.zip,
+                phone: req.body.phone,
+                desc: req.body.desc,
+                type: req.body.type
 
-        }).then(function(results) {
-            res.json(results);
+            })
+            .then(function(results) {
+                res.json(results);
 
-        }).catch(function(err) {
-            //replace with better err handler
-            console.log(err)
-        });
+            })
+            .catch(function(err) {
+                res.status(500);
+            });
     });
 
     // route to delete an unit by id
@@ -133,6 +148,9 @@ module.exports = function(app) {
             })
             .then(function(results) {
                 res.json(results);
+            })
+            .catch(function(err) {
+                res.status(500);
             });
     });
 
@@ -144,12 +162,16 @@ module.exports = function(app) {
 
         // connect to unit model & update it with these obj properties
         db.Units.update(
-            req.body, {
-                where: {
-                    id: req.body.id
-                }
-            }).then(function(dbPost) {
-            res.json(dbPost);
-        });
+                req.body, {
+                    where: {
+                        id: req.body.id
+                    }
+                })
+            .then(function(dbPost) {
+                res.json(dbPost);
+            })
+            .catch(function(err) {
+                res.status(500);
+            });
     });
 };
